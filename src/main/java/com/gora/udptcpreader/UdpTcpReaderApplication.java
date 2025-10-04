b95b8ab88a8d10374df72da17449a7a76520f2fb
SrBruninho <bos.bruno.oliveira55@gmail.com>
2025-10-04 17:20:06 -0300

    Commit inicial do udp-tcp-reader

---
+++ src/main/java/com/gora/udptcpreader/UdpTcpReaderApplication.java
@@ -0,0 +1,13 @@
+package com.gora.udptcpreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdpTcpReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdpTcpReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gora/udptcpreader/UdpTcpReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gora.udptcpreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdpTcpReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
